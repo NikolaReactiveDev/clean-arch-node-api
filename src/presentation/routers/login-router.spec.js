4b058aab175315a539d368abb9457286fd56d37e
nikolareactivedev <nikolareactivedev@gmail.com>
2021-10-10 21:20:09 +0200

    ensure LoginRouter returns 400 if no password is provided as body in httpRequest parameter

--- src/presentation/routers/login-router.spec.js
+++ src/presentation/routers/login-router.spec.js
@@ -1,6 +1,6 @@
 class LoginRouter {
   route (httpRequest) {
-    if (!httpRequest.body.email) {
+    if (!httpRequest.body.email || !httpRequest.body.password) {
       return {
         statusCode: 400
       }
@@ -15,6 +15,18 @@ describe('Login Router', () => {
       // wrong request no email provided
       body: {
         password: 'any_password'
+      }
+    }
+    const httpResponse = sut.route(httpRequest)
+    expect(httpResponse.statusCode).toBe(400)
+  })
+
+  test('should return 400 if no password is provided in body of httpRequest parameter', () => {
+    const sut = new LoginRouter()
+    const httpRequest = {
+      // wrong request no password provided
+      body: {
+        email: 'any_email@gmail.com'
       }
     }
     const httpResponse = sut.route(httpRequest)
