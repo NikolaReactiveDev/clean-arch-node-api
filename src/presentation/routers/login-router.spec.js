a3871f35698bbd25c90fa1b9fd9c730240771ecd
nikolareactivedev <nikolareactivedev@gmail.com>
2021-10-10 20:59:52 +0200

    ensure LoginRouter returns 400 if no email is provided in httpRequest body

---
+++ src/presentation/routers/login-router.spec.js
@@ -0,0 +1,23 @@
+class LoginRouter {
+  route (httpRequest) {
+    if (!httpRequest.body.email) {
+      return {
+        statusCode: 400
+      }
+    }
+  }
+}
+
+describe('Login Router', () => {
+  test('should return 400 if no email is provided in body of httpRequest parameter', () => {
+    const sut = new LoginRouter()
+    const httpRequest = {
+      // wrong request no email provided
+      body: {
+        password: 'any_password'
+      }
+    }
+    const httpResponse = sut.route(httpRequest)
+    expect(httpResponse.statusCode).toBe(400)
+  })
+})
