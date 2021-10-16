c37b8998a702c93d56d6f27cbe896964091feefc
nikolareactivedev <nikolareactivedev@gmail.com>
2021-10-16 06:38:36 +0200

    ensure LoginRouter calls AuthUseCase with correct params

--- src/presentation/routers/login-router.spec.js
+++ src/presentation/routers/login-router.spec.js
@@ -3,12 +3,23 @@ const MissingParamError = require('../helpers/missing-param-error')
 
 /** System under test (SUT) creation with factory pattern */
 const makeSut = () => {
-  return new LoginRouter()
+  class AuthUseCaseSpy {
+    auth (email, password) {
+      this.email = email
+      this.password = password
+    }
+  }
+  const authUseCase = new AuthUseCaseSpy()
+  const sut = new LoginRouter(authUseCase)
+  return {
+    authUseCase,
+    sut
+  }
 }
 
 describe('Login Router', () => {
   test('should return 400 if no email is provided in body of httpRequest parameter', () => {
-    const sut = makeSut()
+    const { sut } = makeSut()
     const httpRequest = {
       // wrong request no email provided
       body: {
@@ -21,7 +32,7 @@ describe('Login Router', () => {
   })
 
   test('should return 400 if no password is provided in body of httpRequest parameter', () => {
-    const sut = makeSut()
+    const { sut } = makeSut()
     const httpRequest = {
       // wrong request no password provided
       body: {
@@ -34,22 +45,29 @@ describe('Login Router', () => {
   })
 
   test('should return 500 if no httpRequest parameter provided', () => {
-    const sut = makeSut()
+    const { sut } = makeSut()
     const httpResponse = sut.route()
     expect(httpResponse.statusCode).toBe(500)
   })
 
   test('should return 500 if httpRequest has no body', () => {
-    const sut = makeSut()
+    const { sut } = makeSut()
     const httpRequest = {}
     const httpResponse = sut.route(httpRequest)
     expect(httpResponse.statusCode).toBe(500)
   })
 
   test('should call AuthUseCase with correct params', () => {
-    const sut = makeSut()
-    const httpRequest = {}
-    const httpResponse = sut.route(httpRequest)
-    expect(httpResponse.statusCode).toBe(500)
+    const { sut, authUseCase } = makeSut()
+    const httpRequest = {
+      // correct request email&password provided
+      body: {
+        email: 'any_email@gmail.com',
+        password: 'any_password'
+      }
+    }
+    sut.route(httpRequest)
+    expect(authUseCase.email).toBe(httpRequest.body.email)
+    expect(authUseCase.password).toBe(httpRequest.body.password)
   })
 })
