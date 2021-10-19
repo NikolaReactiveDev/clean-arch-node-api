626d22ab7fda760c375aba882bc5afa3af41102f
nikolareactivedev <nikolareactivedev@gmail.com>
2021-10-19 07:35:20 +0200

    index on main: a2ebae5 trasnform LoginRouter.route and AuthUseCase.auth to async

--- src/presentation/routers/login-router.js
+++ src/presentation/routers/login-router.js
@@ -1,18 +1,24 @@
 const HttpResponse = require('../helpers/http-response')
+const InvalidParamError = require('../helpers/invalid-param-error')
+const MissingParamError = require('../helpers/missing-param-error')
 
 module.exports = class LoginRouter {
-  constructor (authUseCase) {
+  constructor (authUseCase, emailValidator) {
     this.authUseCase = authUseCase
+    this.emailValidator = emailValidator
   }
 
   async route (httpRequest) {
     try {
       const { email, password } = httpRequest.body
       if (!email) {
-        return HttpResponse.badRequest('email')
+        return HttpResponse.badRequest(new MissingParamError('email'))
+      }
+      if (!this.emailValidator.isValid(email)) {
+        return HttpResponse.badRequest(new InvalidParamError('email'))
       }
       if (!password) {
-        return HttpResponse.badRequest('password')
+        return HttpResponse.badRequest(new MissingParamError('password'))
       }
 
       const accessToken = await this.authUseCase.auth(email, password)
