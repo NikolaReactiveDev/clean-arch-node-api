06b06cef65589f8a44e567b6ee18921f0900b7f4
nikolareactivedev <nikolareactivedev@gmail.com>
2021-10-24 14:12:02 +0200

    ensure TokenGenerator return null if JWT returns null

---
+++ src/utils/helpers/token-generator.spec.js
@@ -0,0 +1,14 @@
+
+class TokenGenerator {
+  async generate (id) {
+    return null
+  }
+}
+
+describe('TokenGenerator', () => {
+  test('should return null if JWT returns null', async () => {
+    const sut = new TokenGenerator()
+    const token = await sut.generate('any_id')
+    expect(token).toBeNull()
+  })
+})
