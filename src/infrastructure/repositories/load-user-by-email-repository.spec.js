3eeeff41a147e7ee05c2fd711cb4ff557b57b396
nikolareactivedev <nikolareactivedev@gmail.com>
2021-10-27 22:32:38 +0200

    move LoadUserByEmailRepository to its own file

--- src/infrastructure/repositories/load-user-by-email-repository.spec.js
+++ src/infrastructure/repositories/load-user-by-email-repository.spec.js
@@ -1,17 +1,18 @@
 const { MongoClient } = require('mongodb')
 
-class LoadUserByEmailRepository {
-  constructor (userModel) {
-    this.userModel = userModel
-  }
+const LoadUserByEmailRepository = require('./load-user-by-email-repository')
 
-  async load (email) {
-    const user = await this.userModel.findOne({ email })
-    return user
-  }
-}
 let connection
 let db
+
+const makeSut = () => {
+  const userModel = db.collection('users')
+  const sut = new LoadUserByEmailRepository(userModel)
+  return {
+    userModel,
+    sut
+  }
+}
 
 describe('LoadUserByEmail Repository', () => {
   beforeAll(async () => {
@@ -31,15 +32,13 @@ describe('LoadUserByEmail Repository', () => {
   })
 
   test('should return null if no user is found', async () => {
-    const userModel = db.collection('users')
-    const sut = new LoadUserByEmailRepository(userModel)
+    const { sut } = makeSut()
     const user = await sut.load('invalid_email@gmail.com')
     expect(user).toBeNull()
   })
 
   test('should return an user if user is found', async () => {
-    const userModel = db.collection('users')
-    const sut = new LoadUserByEmailRepository(userModel)
+    const { sut, userModel } = makeSut()
     await userModel.insertOne({
       email: 'valid_email@gmail.com'
     })
